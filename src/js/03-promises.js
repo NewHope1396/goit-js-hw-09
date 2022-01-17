c2e681d19512dd1e63dc86263402e68618e6d6b8
Viacheslav Aleksandrov <rimondtt@gmail.com>
2022-01-17 23:44:27 +0200

    task-03-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.currentTarget.delay.value);
+  const step = Number(evt.currentTarget.step.value);
+  const amount = Number(evt.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    delay += step;
+
+  };
+
+
+}
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    
+  return new Promise((resolve, reject) => {
+
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+        if (shouldResolve) {
+      resolve({position, delay})
+    } else {
+      reject({position, delay})
+    }
+    }, delay)
+
+  })
 }
